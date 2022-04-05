7e5db6562f3929895fbc81d79b7e63cb9cf85d09
glowing713 <glowing713@naver.com>
2022-04-05 16:54:08 +0900

    feat: Add button state(is active or not) 버튼의 상태에 따라 색상 변경

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,21 @@
 import colors from 'constants/colors';
 import { css } from '@emotion/react';
+import { useState } from 'react';
 
 const Button = ({ children }) => {
+  const [isBtnActive, setIsBtnActive] = useState(false);
+
   return (
     <button
       css={css`
         padding: 7px 15px;
-        background-color: ${colors.white};
+        background-color: ${isBtnActive ? colors.blue : colors.white};
+        color: ${isBtnActive ? colors.white : colors.black};
         border: 1px solid ${colors.grey};
         border-radius: 25px;
         margin-right: 5px;
       `}
+      onClick={() => setIsBtnActive(!isBtnActive)}
     >
       <span>{children}</span>
     </button>
